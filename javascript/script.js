52857430875b2bfe8db7d499dc53c9bfeb22edf2
Jake <hello@jakesmiley.com>
2022-05-14 09:18:58 -0700

    IDK HOW GIT WORKS OMFG

--- javascript/script.js
+++ javascript/script.js
@@ -3,12 +3,8 @@ const API_KEY = "32889f53";
 const searchForm = document.querySelector(".search-form");
 const searchBarInput = document.querySelector(".search-bar__input");
 const searchBar = document.querySelector(".search-bar");
-const greeting = document.querySelector(".greeting");
 const content = document.querySelector(".content");
-
-// This value is so the spinner-start and greeting-fade-out don't overlap
-let spinnerDelay = 375;
-
+const greeting = document.querySelector('.greeting');
 const renderSpinner = function () {
   const spinnerHTML = `
   <svg class="spinner" viewBox="0 0 50 50">
@@ -23,9 +19,8 @@ const renderSpinner = function () {
   </svg>  
   `;
   content.insertAdjacentHTML("afterbegin", spinnerHTML);
-  setTimeout(() => {
-    document.querySelector(".spinner").classList.add("spinner__visible");
-  }, spinnerDelay);
+
+  document.querySelector(".spinner").classList.add("spinner__visible");
 };
 
 const removeSpinner = function () {
@@ -35,8 +30,6 @@ const removeSpinner = function () {
     document.querySelector(".spinner")
       ? document.querySelector(".spinner").remove()
       : "";
-    // Once the greeting is removed, the delay is no longer necessary
-    spinnerDelay = 0;
   }, 376);
 };
 
@@ -72,31 +65,74 @@ const searchMovies = async function (userQuery) {
 const hideGreeting = async function () {
   greeting.classList.remove("greeting__welcome");
   searchForm.classList.remove("search-form__welcome");
-  await wait(270);
 
   greeting.classList.add("greeting__hidden");
+  await wait(320);
+  greeting.style.display = "none";
 };
-
-const showGreeting = function () {
-  clearSearchResults();
+const clearSearchBarInputValue = function () {
   searchBarInput.value = "";
-  greeting.classList.remove("greeting__hidden");
+};
+const clearMovieDetails = async function () {
+  const movieDetails = document.querySelector(".movie-details");
+  if (movieDetails) {
+    movieDetails.classList.add("movie-details__fade-out");
+    await wait(320);
+
+    movieDetails.remove();
+  }
+};
+const clearSearchResults = async function () {
+  const searchResults = Array.from(
+    document.querySelectorAll(".search-results")
+  );
+  if (searchResults) {
+    searchResults.forEach((result) => {
+      result.classList.add("search-results__fade-out");
+    });
+    await wait(320);
+
+    searchResults.forEach((result) => result.remove());
+  }
+};
+const showGreeting = async function () {
+  clearSearchErrorMessages();
+
+  clearSearchResults();
+  clearMovieDetails();
+  clearSearchBarInputValue();
+  greeting.style.display = "flex";
+  await wait(320);
   greeting.classList.add("greeting__welcome");
   searchForm.classList.add("search-form__welcome");
-  // Since the greeting is now back, reset spinner delay
-  spinnerDelay = 375;
+  await wait(320);
+  greeting.classList.remove("greeting__hidden");
 };
 
-const clearSearchErrorMessage = function () {
-  document.querySelector(".error-message__search")
-    ? document.querySelector(".error-message__search").remove()
+const clearSearchErrorMessages = function () {
+  const error = document.querySelector(".error-message__search");
+  error?.remove();
+  const errors = Array.from(
+    document.querySelectorAll(".error-message__search")
+  );
+  errors
+    ? errors.forEach((errorMessage) => {
+      errorMessage.remove();
+    })
     : "";
 };
 const renderSearchError = function () {
+  // fixes bug when navigating quickly to home renders a search error
+  if (window.location.hash === "") return;
+  if (document.querySelector(".error-message__search")) return;
+  console.log("creating search error");
   content.insertAdjacentHTML(
     "afterbegin",
-    `<div class="error-message__search">We couldn't find any movies matching that title. <br> Please try again :)</div>`
+    `<div class="error-message__search error-message__search__visible">We couldn't find any movies matching that title. <br> Please try again :)</div>`
   );
+  document
+    .querySelector(".error-message__search")
+    .classList.add("error-message__search__hidden");
 };
 const renderSearchResults = function (data) {
   let results = "";
@@ -107,7 +143,9 @@ const renderSearchResults = function (data) {
       <li class="search-result">
         <a class="search-result__link" href="#title/${result.imdbID}">
           <div class="search-result__img-box">
-            <img class="search-result__img" src=${result.Poster === "N/A" ? "default-poster.webp" : result.Poster
+            <img class="search-result__img" src=${result.Poster === "N/A"
+        ? "/images/default-poster.webp"
+        : result.Poster
       }>
           </div>
           <div class="search-result__info">
@@ -126,15 +164,6 @@ const renderSearchResults = function (data) {
   content.insertAdjacentHTML("afterbegin", searchResultsHTML);
 };
 
-const clearSearchResults = async function () {
-  const searchResults = document.querySelector(".search-results");
-  if (searchResults) {
-    searchResults.classList.add("search-results__fade-out");
-    await wait(320);
-    searchResults.remove();
-  }
-};
-
 const submitSearch = function (e) {
   e.preventDefault();
   const query = searchBarInput.value;
@@ -143,23 +172,46 @@ const submitSearch = function (e) {
 
 const renderMovieDetails = function (movieData) {
   console.log(movieData);
-  clearSearchResults();
+
+  const movieDetailsHTML = `
+    <div class="movie-details">
+      <div class="movie-details__poster-container">
+        <img class="movie-details__poster" src=${movieData.Poster === "N/A"
+      ? "/images/default-poster.webp"
+      : movieData.Poster
+    }>
+      </div>
+      <div class="movie-details__info>
+        <h3 class="movie-details__title">${movieData.Title}</h3>
+        <div class="movie-details__subtitle">
+          <p class="movie-details__year">${movieData.Year}</p>
+          <p class="movie-details__director">${movieData.Director}</p>
+        </div>
+        <div class="movie-details__runtime-genre-rating">
+          <p class="movie-details__runtime">${movieData.Runtime}</p>
+          <p class="movie-details__genre">${movieData.Genre}</p>
+        </div>
+      </div>
+    </div>
+  `;
+  content.insertAdjacentHTML("afterbegin", movieDetailsHTML);
 };
 
 const wait = (timeToDelay) =>
   new Promise((resolve) => setTimeout(resolve, timeToDelay));
 
-const navigateToSearchResults = async function (query) {
-  // If this is the first search (greeting__welcome class is applied), search bar and arrow slide up, then arrow fades out
-  if (greeting.classList.contains("greeting__welcome")) {
-    hideGreeting();
-    await wait(270);
-  }
+const addQueryToSearchBar = function (query) {
   const search = "%20";
   const replace = " ";
   searchBarInput.value = query.split(search).join(replace);
+};
+
+// this function accepts the query from the url
+const navigateToSearchResults = async function (query) {
+  // Fill in the search bar text with current query (swap '%20' from url with ' ')
+  addQueryToSearchBar(query);
   // Clear search results
-  clearSearchResults();
+
   // spinner fade in
   renderSpinner();
   // await movie data
@@ -168,17 +220,19 @@ const navigateToSearchResults = async function (query) {
   removeSpinner();
   // render search results
   renderSearchResults(data);
-  // results slide up and fade in
-  await wait(75);
-  document
-    .querySelector(".search-results")
-    .classList.add("search-results__fade-in");
+  // waiting for old results to fade out
+  await wait(320);
+  // if there are results, make them visible
+  document.querySelector(".search-results")
+    ? document
+      .querySelector(".search-results")
+      .classList.add("search-results__fade-in")
+    : "";
 };
 
 const navigateToMovieDetails = async function (titleID) {
-  searchBarInput.value = "";
   let SEARCH_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&i=${titleID}`;
-  clearSearchResults();
+
   renderSpinner();
   const response = await fetch(SEARCH_URL);
   const data = await response.json();
@@ -187,12 +241,20 @@ const navigateToMovieDetails = async function (titleID) {
   renderMovieDetails(data);
 };
 
-const navigate = function () {
+const navigate = async function () {
   const hashDirectory = window.location.hash.slice(0, 7);
-  clearSearchErrorMessage();
+  // check for old error message and remove
+
+  clearSearchErrorMessages();
+  if (document.querySelector(".movie-details")) await clearMovieDetails();
+  if (document.querySelector(".search-results")) await clearSearchResults();
   // If hash is empty(navigating back to home page) show greeting
   if (hashDirectory === "") return showGreeting();
-  // check for old error message and remove
+  // If this is the first search (greeting__welcome class is applied), search bar and arrow slide up, then arrow fades out, wait additional time for animations to finish (.32s)
+  if (greeting.classList.contains("greeting__welcome")) {
+    hideGreeting();
+    await wait(320);
+  }
   // If hash is a search query, get data and render search results
   if (hashDirectory === "#search") {
     const query = window.location.hash.slice(8);
@@ -201,9 +263,7 @@ const navigate = function () {
   // If hash is a movie ID, get data and render movie details
   if (hashDirectory === "#title/") {
     const titleID = window.location.hash.slice(7);
-    setTimeout(() => {
-      return navigateToMovieDetails(titleID);
-    }, 300);
+    return navigateToMovieDetails(titleID);
   }
 };
 
